27e293fb332af05f614943a5a6fb6d7137737308
Csriteja <csriteja.1110@gmail.com>
2019-03-18 23:14:55 +0530

    Update logistic_regression.py

--- logistic_regression.py
+++ logistic_regression.py
@@ -9,16 +9,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 
+#Getting Started
 from sklearn.linear_model import LogisticRegression
 df=pd.read_csv('logistic_regression_df.csv')
 df.columns = ['X','Y']
 df.head()
 
-sns.set_context("notebook",font_scale=1.1)
-sns.set_style("ticks")
-sns.regplot('X','Y',data=df,logistic=True)
-plt.ylabel('Probability')
-plt.xlabel('Explanatory')
+#Implementation
 
 logistic=LogisticRegression()
 X=(np.asarray(df.X)).reshape(-1,1)
@@ -28,3 +25,11 @@ logistic.score(X,Y)
 print('Coefficient: \n',logistic.coef_)
 print('Intercept: \n',logistic.intercept_)
 print('R^2 value: \n',logistic.score(X,Y))
+
+#Visualization
+
+sns.set_context("notebook",font_scale=1.1)
+sns.set_style("ticks")
+sns.regplot('X','Y',data=df,logistic=True)
+plt.ylabel('Probability')
+plt.xlabel('Explanatory')
