7c283b500c07328b12b51a88803e06a140244942
Csriteja <csriteja.1110@gmail.com>
2019-03-18 23:15:18 +0530

    Update linear_regression.py

--- linear_regression.py
+++ linear_regression.py
@@ -10,16 +10,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 
+#Getting Started
 from sklearn import linear_model
 df=pd.read_csv('linear_regression_df.csv')
 df.columns = ['X','Y']
 df.head()
 
-sns.set_context("notebook", font_scale=1.1)
-sns.set_style("ticks")
-sns.lmplot('X','Y',data=df)
-plt.ylabel('Response')
-plt.xlabel('Explanatory')
+#Implementation
 
 linear = linear_model.LinearRegression()
 trainX=np.asarray(df.X[20:len(df.X)]).reshape(-1,1)
@@ -34,3 +31,11 @@ print('R_square value: \n', score)
 predicted = linear.predict(testX)
 print(predicted)
 print(testY)
+
+#Visualization
+
+sns.set_context("notebook", font_scale=1.1)
+sns.set_style("ticks")
+sns.lmplot('X','Y',data=df)
+plt.ylabel('Response')
+plt.xlabel('Explanatory')
