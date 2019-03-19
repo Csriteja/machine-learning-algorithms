2f129578bb003e5d502f1f938852a45273db24b0
Csriteja <csriteja.1110@gmail.com>
2019-03-19 23:56:10 +0530

    Update SVM.py

--- SVM.py
+++ SVM.py
@@ -16,3 +16,13 @@ df.columns = ['X4', 'X3', 'X1', 'X2', 'Y']
 df = df.drop(['X4', 'X3'], 1)
 df.head()
 
+#Implementation
+from sklearn.cross_validation import train_test_split
+support = svm.SVC()
+X = df.values[:, 0:2]
+Y = df.values[:, 2]
+trainX, testX, trainY, testY = train_test_split(X, Y, test_size=0.3)
+support.fit(trainX, trainY)
+print('Accuracy: \n', support.score(testX, testY))
+pred = support.predict(testX)
+
