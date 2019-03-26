35cf6a098836128e4966eeacff72e77381c42ddc
Csriteja <csriteja.1110@gmail.com>
2019-03-27 00:02:54 +0530

    Update KNN.py

--- KNN.py
+++ KNN.py
@@ -28,3 +28,9 @@ neighbors.fit(trainX,trainY)
 print('Accuracy: \n', neighbors.score(testX,testY))
 pred=neighbors.predict(testX)
 
+#Visualization
+sns.set_context("notebook", font_scale=1.1)
+sns.set_style("ticks")
+sns.lmplot('X1','X2',scatter=True, fit_reg=False, data=df, hue='Y')
+plt.ylabel('X2')
+plt.xlabel('X1')
