df2ab6d047325d1a7f4495d17eb5052f029f13f3
Csriteja <csriteja.1110@gmail.com>
2019-03-26 23:57:54 +0530

    Update KNN.py

--- KNN.py
+++ KNN.py
@@ -5,3 +5,26 @@ Created on Tue Mar 26 23:46:27 2019
 @author: csrit
 """
 
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+
+#Getting Started
+from sklearn.neighbors import KNeighborsClassifier
+df = pd.read_csv('iris_df.csv')
+df.columns = ['X1', 'X2','X3', 'X4','X5']
+df = df.drop(['X4','X3'],1)
+df.head()
+
+#Implementation
+from sklearn.model_selection import train_test_split
+neighbors = KNeighborsClassifier(n_neighbors=5)
+X=df.values[:,0:2]
+Y=df.values[:,2]
+trainX,testX, trainY, testY = train_test_split(X,Y,test_size=0.3)
+
+neighbors.fit(trainX,trainY)
+print('Accuracy: \n', neighbors.score(testX,testY))
+pred=neighbors.predict(testX)
+
