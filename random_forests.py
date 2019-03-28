25f234c75e389a345479e9be4c0984d36f379339
Csriteja <csriteja.1110@gmail.com>
2019-03-28 23:45:47 +0530

    Create random_forests.py

---
+++ random_forests.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 28 23:45:07 2019
+
+@author: csrit
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+
