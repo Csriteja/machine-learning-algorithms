d18350969c8191192761dca11071c1521a67063f
Csriteja <csriteja.1110@gmail.com>
2019-03-28 23:44:59 +0530

    Update KNN.py

--- KNN.py
+++ KNN.py
@@ -17,6 +17,13 @@ df.columns = ['X1', 'X2','X3', 'X4','Y']
 df = df.drop(['X4','X3'],1)
 df.head()
 
+#Visualization
+sns.set_context("notebook", font_scale=1.1)
+sns.set_style("ticks")
+sns.lmplot('X1','X2',scatter=True, fit_reg=False, data=df, hue='Y')
+plt.ylabel('X2')
+plt.xlabel('X1')
+
 #Implementation
 from sklearn.model_selection import train_test_split
 neighbors = KNeighborsClassifier(n_neighbors=5)
@@ -28,9 +35,4 @@ neighbors.fit(trainX,trainY)
 print('Accuracy: \n', neighbors.score(testX,testY))
 pred=neighbors.predict(testX)
 
-#Visualization
-sns.set_context("notebook", font_scale=1.1)
-sns.set_style("ticks")
-sns.lmplot('X1','X2',scatter=True, fit_reg=False, data=df, hue='Y')
-plt.ylabel('X2')
-plt.xlabel('X1')
+
