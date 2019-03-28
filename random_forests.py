9800afac40843767361112a373666f8d147cb0dc
Csriteja <csriteja.1110@gmail.com>
2019-03-28 23:47:39 +0530

    Update random_forests.py

--- random_forests.py
+++ random_forests.py
@@ -10,3 +10,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 
+#Getting Started
+from sklearn.ensemble import RandomForestClassifier
+df = pd.read_csv('iris_df.csv')
+df.columns = ['X1', 'X2', 'X3', 'X4', 'Y']
+df.head()
+
